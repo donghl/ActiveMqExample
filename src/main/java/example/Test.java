437988312d08c749620d0a5bdf28929ec3462ee8
donghl <feifei2001@me.com>
2015-08-25 17:20:24 +0800

    S

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -2,6 +2,7 @@ package example;
 
 import java.net.*;
 public class Test {
+	
    public static void main(String[] args) throws Exception {
         String ip = InetAddress.getLocalHost().getHostAddress();
         InetAddress[] ip1 = InetAddress.getAllByName(InetAddress.getLocalHost().getHostName()); 
