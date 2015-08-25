4f769679e663bcbfcbc2ed5f8b8f2a44613cdd7f
donghl <feifei2001@me.com>
2015-08-25 17:16:07 +0800

    得到多ip

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -4,6 +4,12 @@ import java.net.*;
 public class Test {
    public static void main(String[] args) throws Exception {
         String ip = InetAddress.getLocalHost().getHostAddress();
+        InetAddress[] ip1 = InetAddress.getAllByName(InetAddress.getLocalHost().getHostName()); 
         System.out.println(ip);
+        for(InetAddress ipaddress :ip1)
+        {
+        	System.out.println(ipaddress.getHostAddress());
+        }
+        
    }
 }
